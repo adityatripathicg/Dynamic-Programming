e6e60c90781549e9e4a5a9cd5ac377341c22f3f9
Aditya Tripathi <adityatripathi3906@gmail.com>
2024-05-01 13:13:54 +0530

    climbingStairs DP

---
+++ climbingStairs.java
@@ -0,0 +1,29 @@
+public class climbingStairs {
+        public int climbStairs(int n) {
+            // Handle the base cases
+            if(n == 1 || n == 2){
+                return n;
+            }
+            else if(n == 0 ){
+                return 1;
+            }
+            else if(n<0){
+                return 0;
+            }
+            
+            // Create a dp array to store the number of ways to reach each step
+            int dp[] = new int[n+1];
+            
+            // Initialize the base cases
+            dp[1] = 1;
+            dp[2] = 2;
+            
+            // Fill the dp array using the recurrence relation
+            for(int i=3; i<=n; i++){
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+            
+            // Return the number of ways to reach the n-th step
+            return dp[n];
+        }
+    }
